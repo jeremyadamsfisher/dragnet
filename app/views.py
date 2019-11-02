dd62ec52cb9bb005d3917a95bd18c8f47544372b
Jeremy Fisher <jeremy@adamsfisher.me>
2019-11-02 10:13:10 -0400

    add basic file upload capabilities

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
-from flask import render_template
+import os
+import tempfile
+from flask import render_template, redirect, url_for, request
 from app import app
 
-@app.route('/')
+
+@app.route("/")
 def main_page():
     return render_template("main.html")
+
+
+@app.route('/upload', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']
+        with tempfile.NamedTemporaryFile() as f_temp:
+            f.save(f_temp.name)
+            return render_template("success.html", name = f_temp.name)
