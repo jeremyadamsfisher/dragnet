6597d3d6d98779c21d2c2d444fee1f132a92bc20
Jeremy Fisher <jeremy@adamsfisher.me>
2019-12-17 08:33:11 -0500

    refactor views

---
+++ app/constants.py
@@ -0,0 +1,11 @@
+BRANDING_TAGLINES = [
+    "GAN i get an amen?",
+    "drag up your machine learning",
+    "born naked and the rest is GAN",
+    "style transfer for drag queens"
+]
+
+TAGS = [
+    "Tens! Tens across the board!",
+    "America, shes stunning!",
+]
--- app/views.py
+++ app/views.py
@@ -3,72 +3,61 @@ import tempfile
 import io
 import random
 import pathlib
-import PIL
 import uuid
+import PIL
 
 from pathlib import Path
-from threading import Thread
 from google.cloud import storage, tasks_v2
 from flask import render_template, redirect, url_for, request
-from .translate import translate
 
+from .translate import translate
+from .constants import BRANDING_TAGLINES, TAGS
 from app import app
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath("./secrets.json")
 storage_client = storage.Client()
 client = tasks_v2.CloudTasksClient()
-parent = client.queue_path("dragnet", "us-east1", "dragnet-queue")
+parent = client.queue_path("dragnet", "us-east1", "dragnet-queue")  # todo: put this in an .env
 
-branding_taglines = [
-    "GAN i get an amen?",
-    "drag up your machine learning",
-    "born naked and the rest is GAN",
-    "style transfer for drag queens"
-]
-tags = [
-    "Tens! Tens across the board!",
-    "America, shes stunning!",
-]
-
-
-gallery_imgs = []
+GALLERY_IMGS = []
 for comparison_dir in pathlib.Path("./app/static/imgs").glob("*"):
-    if not comparison_dir.is_dir():
-        continue
-    else:
-        gallery_imgs.append((f"imgs/{comparison_dir.name}/norm.jpg", f"imgs/{comparison_dir.name}/drag.jpg"))
-
-
-original_render_template = render_template
-def render_template_(*args, **kwargs):
-    """wrapper for flask render template that fills in the branding"""
-    return original_render_template(
-        *args,
-        **kwargs,
-        branding_tagline=random.choice(branding_taglines),
-        gallery_imgs=gallery_imgs
-    )
-render_template = render_template_
+    if comparison_dir.is_dir():
+        GALLERY_IMGS.append((
+            f"imgs/{comparison_dir.name}/norm.jpg",
+            f"imgs/{comparison_dir.name}/drag.jpg"
+        ))
 
 
 @app.route("/")
 def main():
-    """landing page"""
-    return render_template("main.html")
+    """view method: landing page -- insert the gallery images and
+    branding tagline"""
+    return render_template(
+        "main.html",
+        gallery_imgs=GALLERY_IMGS,
+        branding_tagline=random.choice(BRANDING_TAGLINES),
+    )
 
 
 def fail(msg):
-    """convenience failure state"""
-    return render_template("app_failure.html", message=msg)
+    """view method: convenience failure state -- display
+    failure to user
+    
+    Arguments:
+        msg: message to display
+    """
+    return render_template(
+        "app_failure.html",
+        message=msg
+    )
 
 
 @app.route("/result", methods=["POST"])
 def result():
-    """result page; enqueue the prediction"""
+    """view method: result page -- upload the user profile image, enqueue
+    to cloud tasks and redirect user to the apropriate page"""
     if request.method == "POST":
-        
         profile_img = request.files["file"]
-
         if "heic" in profile_img.filename.lower():
             return fail(f"could not translate {profile_img.filename}")
         else:
@@ -81,11 +70,10 @@ def result():
                 }
             }
             client.create_task(parent, task)
-
             return render_template(
                 "app_result.html",
                 img_id=img_id,
-                tag=random.choice(tags)
+                tag=random.choice(TAGS)
             )
 
 
