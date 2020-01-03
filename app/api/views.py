f7ad69f836d6456a60925333c141d17aa5804ad3
Jeremy Fisher <jeremy@adamsfisher.me>
2020-01-03 17:25:40 -0600

    remove code litter and move views out of module declaration for blueprints

---
+++ app/api/views.py
@@ -0,0 +1,75 @@
+from . import api
+
+import requests
+import PIL
+import tempfile
+import io
+import uuid
+
+from os import path
+from flask import (
+    url_for,
+    request,
+    make_response,
+    jsonify,
+    abort,
+    current_app as app
+)
+
+from . import utils
+from .machine_learning import translate
+
+
+@api.route("/enqueue", methods=["POST"])
+def enqueue():
+    """view method: upload the user profile image, enqueue
+    to cloud tasks send back redirect information"""
+    if request.method == "POST":
+        payload = request.get_data()
+        if not payload:
+            abort(400)  # bad request
+        else:
+            img_id = str(uuid.uuid4())
+            with tempfile.TemporaryDirectory() as t_dir:
+                fp = path.join(t_dir, "in.jpg")
+                try:
+                    img = PIL.Image.open(io.BytesIO(payload)).convert("RGB")
+                    resized_img = utils.resize_image(img)
+                except OSError:
+                    abort(415)  # image that PIL cannot ingest
+                resized_img.save(fp, "JPEG")
+                utils.upload_gcp_bucket(fp, img_id, app.config["GCLOUD_INTERMEDIARY_BUCKET"])
+            # run the machine learning task on a worker using gcloud tasks
+            utils.create_gcloud_task("GET", url_for(".predict", img_id=img_id))
+            j = {"result": url_for(".checkprogress", img_id=img_id)}
+
+            return make_response(jsonify(j), 200)
+
+
+@api.route("/checkprogress/<img_id>")
+def checkprogress(img_id: str):
+    """ping cloud tasks to see if the image is cooked"""
+    drag_bucket = app.config["GCLOUD_DRAG_BUCKET"]
+    url = f"https://storage.googleapis.com/{drag_bucket}/{img_id}"
+    r = requests.get(url)
+    if r.status_code == 404:
+        return make_response(jsonify({"status": "loading"}))
+    else:
+        return make_response(jsonify({"status": "done", "url": url}))
+
+
+@api.route("/predict/<img_id>")
+def predict(img_id: str):
+    """predict what someone looks like in drag and upload it to gcp
+
+    Arguments:
+        img_id: how we will save the file to gcp, front-end also knows about
+    """
+    with tempfile.TemporaryDirectory() as t_dir:
+        fp_in = path.join(t_dir, "in.jpg")
+        fp_out = path.join(t_dir, "out.jpg")
+        utils.download_gcp_bucket(fp_in, img_id, app.config["GCLOUD_INTERMEDIARY_BUCKET"])
+        img = PIL.Image.open(fp_in).convert("RGB")
+        img = translate(img)
+        img.save(fp_out, format="JPEG")
+        utils.upload_gcp_bucket(fp_out, img_id, app.config["GCLOUD_DRAG_BUCKET"])
--- app/frontend/__init__.py
+++ app/frontend/__init__.py
@@ -1,32 +1,8 @@
-import random
-from pathlib import Path
-from flask import render_template, Blueprint
-
-from .constants import BRANDING_TAGLINES, RESULT_TAGS
-
+from flask import Blueprint
 webfrontend = Blueprint(
     "frontend",
     __name__,
     template_folder="templates",
     static_folder="static"
 )
-
-GALLERY_IMGS = []
-for comparison_dir in Path("./app/static/frontend/imgs").glob("*"):
-    if comparison_dir.is_dir():
-        GALLERY_IMGS.append((
-            f"frontend/imgs/{comparison_dir.name}/norm.jpg",
-            f"frontend/imgs/{comparison_dir.name}/drag.jpg"
-        ))
-
-@webfrontend.route("/")
-def main():
-    """view method: landing page -- insert the gallery images and
-    branding tagline"""
-    return render_template(
-        "frontend/app.html",
-        gallery_imgs=GALLERY_IMGS,
-        loadingLine="uploading lerk...",
-        loadedLine=random.choice(RESULT_TAGS),
-        branding_tagline=random.choice(BRANDING_TAGLINES),
-    )
+from .views import *
