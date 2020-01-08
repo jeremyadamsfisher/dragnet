acea07eb1af055cdc2b2ec1d7f8e98a98bc703da
jeremyadamsfisher <jeremy.adams.fisher@gmail.com>
2020-01-08 16:53:10 -0500

    Add logic to test on local iphone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run()
+    if os.environ.get("DRAGNET_DEPLOYMENT", None) == "local":
+        app.run(host="0.0.0.0")
+    else:
+        app.run()
